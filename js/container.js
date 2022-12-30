2b0524b51792a427da0ca6b3bbaf1d0dba1fc2b8
Serhii Myshko <sergeiown@gmail.com>
2022-12-31 01:44:42 +0200

    add show list of songs function

--- js/container.js
+++ js/container.js
@@ -1,4 +1,41 @@
-import * as minimum from "./minimum.js";
-import * as normal from "./normal.js";
-import * as maximum from "./maximum.js";
+const playList = [
+  {
+    author: "LED ZEPPELIN",
+    song: "STAIRWAY TO HEAVEN",
+  },
+  {
+    author: "QUEEN",
+    song: "BOHEMIAN RHAPSODY",
+  },
+  {
+    author: "LYNYRD SKYNYRD",
+    song: "FREE BIRD",
+  },
+  {
+    author: "DEEP PURPLE",
+    song: "SMOKE ON THE WATER",
+  },
+  {
+    author: "JIMI HENDRIX",
+    song: "ALL ALONG THE WATCHTOWER",
+  },
+  {
+    author: "AC/DC",
+    song: "BACK IN BLACK",
+  },
+  {
+    author: "QUEEN",
+    song: "WE WILL ROCK YOU",
+  },
+  {
+    author: "METALLICA",
+    song: "ENTER SANDMAN",
+  },
+];
 
+localStorage.setItem(
+  "storageItem",
+  JSON.stringify(playList)
+); /* add playlist to local storage as a string */
+
+import * as normal from "./normal.js";
--- js/normal.js
+++ js/normal.js
@@ -1,1 +1,19 @@
+function showListOfSongs() {}
+const sourceArray = JSON.parse(localStorage.storageItem);
 
+const songsList = sourceArray.map(
+  (item) => item["song"]
+); /* make array of songs only */
+
+const uniqSortedSongsList = Array.from(
+  new Set(songsList)
+).sort(); /* make array of sorted unique songs */
+
+let liValues = "";
+uniqSortedSongsList.forEach(
+  (elem) => (liValues += `<li>${elem}</li>`)
+); /* make <ol class"list"> inner tags from the array of sorted unique songs */
+
+document.querySelector(".list").innerHTML = liValues;
+
+export {};
